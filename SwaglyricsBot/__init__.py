208eac3cc4ad613dddf83466dd6a1edb2305338f
Frytek <flubbet@gmail.com>
2019-12-29 18:56:13 +0100

    Exceptions handling

--- SwaglyricsBot/__init__.py
+++ SwaglyricsBot/__init__.py
@@ -1,2 +1,22 @@
 name = 'SwaglyricsBot'
 __version__ = '0.0.1'
+
+
+class LyricsError(Exception):
+
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class LyricsNotFound(LyricsError):
+
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class SpotifyClosed(LyricsError):
+
+    def __init__(self, message="You are not listening to anything or Spotify is not connected to discord! \n "
+                               "Make sure you have enabled status in Settings -> Connections -> Spotify -> Display "
+                               "Spotify as your status"):
+        super().__init__(message)
--- SwaglyricsBot/swaglyrics_bot.py
+++ SwaglyricsBot/swaglyrics_bot.py
@@ -6,6 +6,7 @@ from discord import Spotify
 from discord.ext import commands
 from discord.ext.commands.help import MinimalHelpCommand
 from swaglyrics import cli as swaglyrics
+from SwaglyricsBot import LyricsNotFound, SpotifyClosed, LyricsError
 
 bot = commands.Bot(command_prefix="$", help_command=MinimalHelpCommand())
 
@@ -13,31 +14,34 @@ bot = commands.Bot(command_prefix="$", help_command=MinimalHelpCommand())
 def get_spotify_data(user):
     spotify_activity = [activity for activity in user.activities if isinstance(activity, Spotify)]
     if len(spotify_activity) == 0:
-        raise AttributeError
+        raise SpotifyClosed()
     return spotify_activity[0].title, spotify_activity[0].artist
 
 
 @bot.command(name='swaglyrics')
-async def get_lyrics(ctx, song=None, artist=None):
+async def get_lyrics_command(ctx, song=None, artist=None):
     try:
         if song is None and artist is None:
             song, artist = get_spotify_data(ctx.author)
         debug_string = "Getting lyrics for {} - {}".format(song, artist)
         print(debug_string)
         await ctx.send(debug_string)
-        lyrics = swaglyrics.get_lyrics(song, artist)
+        lyrics = get_lyrics(song, artist)
         splitted_lyrics = chop_string_into_chunks(lyrics, 1024)
         embed = discord.Embed()
         embed.title = "Lyrics for {} - {}".format(song, artist)
         for chunk in splitted_lyrics:
             embed.add_field(name=u"\u200C", value=chunk, inline=False)
         await ctx.send(embed=embed)
-    except AttributeError:
-        await ctx.send("You are not listening to anything or Spotify is not connected to discord! \n "
-                       "Make sure you have enabled status in Settings -> Connections -> Spotify -> Display "
-                       "Spotify as your status")
-    except TypeError:
-        await ctx.send("Lyrics for {} - {} not found in genius database.".format(song, artist))
+    except LyricsError as ex:
+        await ctx.send(ex)
+
+
+def get_lyrics(song, artist):
+    lyrics = swaglyrics.get_lyrics(song, artist)
+    if lyrics is None:
+        raise LyricsNotFound("Lyrics for {} - {} not found in genius database.".format(song, artist))
+    return lyrics
 
 
 def chop_string_into_chunks(string, chunk_size):
