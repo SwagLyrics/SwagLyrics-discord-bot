50d05b60c913981db80c223e57bba2c5b8c6dded
Srevin Saju <srevinsaju@sugarlabs.org>
2020-07-17 13:04:24 +0300

    Remove unnecessary alias for the psutil module
    The `psutil` module was imported with the same namespace, i.e `import psutil as psutil` which is equivalent to `import psutil` in theory

--- SwagLyricsBot/dev_commands.py
+++ SwagLyricsBot/dev_commands.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from random import getrandbits
 
 import discord
-import psutil as psutil
+import psutil
 from discord.ext import commands
 
 process = psutil.Process()
