bc6477a02b27a37f4ec0bb05bc8e04cbfc0d2add
Pranav <58635215+Vyprath@users.noreply.github.com>
2020-11-03 22:40:49 +0530

    Added AllowedMentions so the bot won't mention @everyone
    Allowed Mentions has been added so the bot cannot be tricked into using @everyone, @here or any other mention.

--- SwagLyricsBot/swaglyrics_bot.py
+++ SwagLyricsBot/swaglyrics_bot.py
@@ -10,7 +10,8 @@ from SwagLyricsBot.general_commands import GeneralCommands
 from SwagLyricsBot.links_commands import LinksCommands
 from SwagLyricsBot.topGG import TopGG
 
-bot = commands.Bot(command_prefix=when_mentioned_or("$"), help_command=None)
+bot = commands.Bot(command_prefix=when_mentioned_or("$"), help_command=None,
+                   allowed_mentions=discord.AllowedMentions(roles=False, users=False, everyone=False)
 
 
 def find_mutual_guild(user_id):
